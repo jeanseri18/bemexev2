87a9bd7d07ba570a41b71b66c5e6e68300da9fad
jeanseri18 <jeanseri18@outlook.com>
2024-09-24 15:46:05 +0000

    rr

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -4,13 +4,16 @@
 
 @section('content')
 <br><br>
+<div style="margin:20px;" >
 
-<div class="container mt-5">
+<div class="">
 
 <div class="card">
         <div class="row no-gutters">
             <!-- Colonne avec image -->
             <div class="col-md-6">
+            <br>
+            
                 <img src="../../assets/1554450685082.jpeg" class="card-img w-100" style="  background-size: cover;"
                     alt="Image">
 
@@ -20,7 +23,7 @@
                 <div class="card-body">
                 <h5 class="card-title"><span class="text-primary">Presentation</span></h5>
                 <h2 class="card-title display-3">BEM Executive Education</h2>
-                    <ul class="list-unstyled mb-5" style="font-size:14PX">
+                    <ul class="list-unstyled " style="font-size:14PX">
                         <li class="mb-2">
 
                             <span class="ms-2"><h3 >Dr Pape Madické DIOP Président Directeur Général BEM </h3>
@@ -32,8 +35,8 @@
                             <span class="ms-2">Avec le même objectif de montée en compétences et la même exigence d’excellence, BEM Executive Education souhaite accompagner aujourd’hui vos collaborateurs tout au long de leur carrière afin de leur permettre de progresser et de contribuer de manière croissante au développement, à la stratégie de votre organisation. En s’appuyant sur un panel de consultants internationaux expérimentés, BEM Executive Education intervient notamment dans les domaines de l’efficacité managériale, de la communication maîtrisée, du développement personnel et de la gouvernance. Grâce à ses séminaires et ses programmes certifiants, BEM Executive Education accompagne vos collaborateurs et les installe dans une culture de performance et de responsabilité.</span>
                         </li>
                         <li class="mb-2">
-
-                            <span class="ms-2">Notre philosophie réside cependant dans notre capacité à proposer des programmes sur mesure, construits sur la base des besoins spécifiques de chaque organisation. Notre méthodologie place le client au cœur du processus de définition, de validation et d’évaluation du parcours de formation. En complément du transfert de connaissances et de compétences, BEM Executive Education s’attache à communiquer à vos collaborateurs des valeurs éthiques et comportementales indispensables à la formation de collaborateurs responsables.<big>"</big></span><br><br>
+                        Notre philosophie réside cependant dans notre capacité à proposer des programmes sur mesure, construits sur la base des besoins spécifiques de chaque organisation. Notre méthodologie place le client au cœur du processus de définition, de validation et d’évaluation du parcours de formation. En complément du transfert de connaissances et de compétences, BEM Executive Education s’attache à communiquer à vos collaborateurs des valeurs éthiques et comportementales indispensables à la formation de collaborateurs responsables.
+                            <span class="ms-2"><big>"</big></span><br><br>
                             
 
                         </li>
@@ -45,7 +48,7 @@
     </div>
     
 </div>
-<div class="container ">
+<div class=" ">
 
     <br><br>
     <br>
